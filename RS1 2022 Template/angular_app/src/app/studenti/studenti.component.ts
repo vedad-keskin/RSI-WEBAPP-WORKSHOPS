a3cb47991596f3be6e0e6735b4192f92da5b3db2
Vedad <vedad.ribery@gmail.com>
2024-02-03 21:22:59 +0100

    Dodavanje template

---
+++ RS1 2022 Template/angular_app/src/app/studenti/studenti.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MojConfig} from "../moj-config";
+import {Router} from "@angular/router";
+declare function porukaSuccess(a: string):any;
+declare function porukaError(a: string):any;
+
+@Component({
+  selector: 'app-studenti',
+  templateUrl: './studenti.component.html',
+  styleUrls: ['./studenti.component.css']
+})
+export class StudentiComponent implements OnInit {
+
+  title:string = 'angularFIT2';
+  ime_prezime:string = '';
+  opstina: string = '';
+  studentPodaci: any;
+  filter_ime_prezime: boolean;
+  filter_opstina: boolean;
+
+
+  constructor(private httpKlijent: HttpClient, private router: Router) {
+  }
+
+  testirajWebApi() :void
+  {
+    this.httpKlijent.get(MojConfig.adresa_servera+ "/Student/GetAll", MojConfig.http_opcije()).subscribe(x=>{
+      this.studentPodaci = x;
+    });
+  }
+
+  ngOnInit(): void {
+    this.testirajWebApi();
+  }
+
+}
